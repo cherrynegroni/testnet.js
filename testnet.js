e2d1221bad43893a4f77c0870fa910dd47eacb64
cherrynegroni <101512390+cherrynegroni@users.noreply.github.com>
2022-03-14 23:04:58 +0300

    coding for nosana

---
+++ testnet.js
@@ -0,0 +1,9 @@
+var myText="General variable"
+document.write(myText+"<br>")
+function show(){
+var myText
+myText="Local variable"
+document.write(myText+"<br>")
+}
+show()
+document.write(myText+"<br>")
